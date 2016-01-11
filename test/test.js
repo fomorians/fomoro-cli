9c2de2b8b4b47f4f4ac25a7c299b253f149dc2a7
Jim Fleming <jimmy.fleming@gmail.com>
2016-01-11 09:25:21 -0800

    Refactor project and adjust CLI UX

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var should = require('should');
+
+describe('fomoro', () => {
+  describe('download', () => {
+    it('should ...', () => {
+    });
+  });
+
+  describe('upload', () => {
+    it('should ...', () => {
+    });
+  });
+
+  describe('train', () => {
+    it('should ...', () => {
+    });
+  });
+});
